0825277c01dfcc4d18c1637779a01c22e55570e0
Joao Vitor <jvsantospessoal@gmail.com>
2024-11-06 02:16:26 -0300

    2 - Criando os indices.

--- index.sql
+++ index.sql
@@ -1,2 +1,12 @@
+DROP INDEX IF EXISTS indice_pokemon;
+
+INSERT INTO pokemon (nome)
+SELECT 'Pokemon ' || generate_series(1, 1000000);
+
+EXPLAIN ANALYZE
+SELECT * FROM pokemon WHERE nome = 'Pokemon 9234100';
+
 CREATE INDEX indice_pokemon ON pokedex.pokedex_schema.pokemon USING BTREE (id, nome);
 
+EXPLAIN ANALYZE
+SELECT * FROM pokemon WHERE nome = 'Pokemon 9634210';
